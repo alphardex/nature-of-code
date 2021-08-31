c1473ed492107683ff3762bb201bd9705fd45424
alphardex <2582347430@qq.com>
2021-08-31 08:39:26 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="relative min-h-screen">
+    <ul>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+});
+</script>
+
+<style lang="scss" scoped></style>
